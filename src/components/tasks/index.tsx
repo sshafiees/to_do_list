bd931b01b0fc37475d9a6cffef005a4d71f63737
sina <alphaproject131313@gmail.com>
2025-09-18 19:40:53 +0330

    mocks

--- src/components/tasks/index.tsx
+++ src/components/tasks/index.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
 import TaskItem from './taskItem';
+import { taskList } from '../../mocks/tasks';
 
 export default function TasksList() {
   return (
-    <>
-      <TaskItem
-        taskId="1"
-        title="عنوان تسک"
-        description="توضیحات تسک"
-        dueDate={new Date(Date.now() + 86400000)} // date +1 day
-        createdAt={new Date(Date.now() - 86400000)}
-        priority="high"
-        status="inProgress"
-        category="توسعه"
-      />
-      <TaskItem
-        taskId="2"
-        title="عنوان تسک 2"
-        description="توضیحات تسک 2"
-        dueDate={new Date(Date.now() + 86400000)} // date +1 day
-        createdAt={new Date(Date.now() - 86400000)}
-        priority="medium"
-        status="todo"
-        category="توسعه"
-      />
-      <TaskItem
-        taskId="3"
-        title="عنوان تسک 3"
-        description="توضیحات تسک 3"
-        dueDate={new Date(Date.now() + 86400000)} // date +1 day
-        createdAt={new Date(Date.now() - 86400000)}
-        priority="low"
-        status="done"
-        category="توسعه"
-      />
-    </>
+    <div>
+      {taskList.map(task => (
+        <TaskItem
+          key={task.taskId}
+          taskId={task.taskId}
+          title={task.title}
+          description={task.description}
+          dueDate={task.dueDate}
+          createdAt={task.createdAt}
+          priority={task.priority}
+          status={task.status}
+          category={task.category}
+        />
+      ))}
+    </div>
   );
 }
