a89040b5c6ecbc99e8f7a6e59936e2c77d5e1055
sina <alphaproject131313@gmail.com>
2025-09-18 09:58:15 +0330

    task-item

---
+++ src/components/tasks/index.js
@@ -0,0 +1,40 @@
+import TaskItem from './taskItem';
+
+export default function TasksList() {
+  return (
+    <>
+      <TaskItem
+        taskId="1"
+        title="عنوان تسک 1"
+        description="توضیحات تسک 1"
+        dueDate={new Date(Date.now() + 86400000)} // date +1 day
+        createdAt={new Date(Date.now() - 86400000)}
+        priority="high"
+        status="inProgress"
+        category="توسعه"
+      />
+
+      <TaskItem
+        taskId="2"
+        title="عنوان تسک 2"
+        description="توضیحات تسک 2"
+        dueDate={new Date(Date.now() + 86400000)} // date +1 day
+        createdAt={new Date(Date.now() - 86400000)}
+        priority="medium"
+        status="todo"
+        category="تست"
+      />
+
+      <TaskItem
+        taskId="3"
+        title="عنوان تسک 3"
+        description="توضیحات تسک 3"
+        dueDate={new Date(Date.now() + 86400000)} // date +1 day
+        createdAt={new Date(Date.now() - 86400000)}
+        priority="low"
+        status="done"
+        category="پشتیبانی"
+      />
+    </>
+  );
+}
