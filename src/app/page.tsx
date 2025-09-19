dcdfc2d03dd25b6fb3d89d6c7e1477adf8167a56
sina <alphaproject131313@gmail.com>
2025-09-19 12:11:01 +0330

    dynamic1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
-// src/app/page.tsx
+'use client';
+import React, { useState } from 'react';
 import Search from '../components/search';
 import Filters from '../components/filters';
 import Overview from '../components/overview';
 import TasksList from '../components/tasks'; // چون داخل پوشه tasks فایل index.tsx داری
 
 export default function Home() {
+  const [searchText, setSearchText] = useState<string>('');
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
       <div className="container h-full">
         <div className="grid gap-4">
           <Filters />
-          <Search />
+          <Search searchText={searchText} setSearchText={setSearchText} />
           <Overview />
-          <TasksList />
+          <TasksList searchText={searchText} />
         </div>
       </div>
     </div>
--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,13 +1,22 @@
+'use client';
 import React from 'react';
 import Block from '../layout/block';
 
-export default function Search() {
+type searchProps = {
+  searchText: string;
+  setSearchText: React.Dispatch<React.SetStateAction<string>>;
+};
+export default function Search({ searchText, setSearchText }: searchProps) {
   return (
     <Block>
       <input
         type="text"
         placeholder="جستجو..."
         className="border border-gray-300 rounded-md p-2 w-full"
+        value={searchText}
+        onChange={event => {
+          setSearchText(event.target.value);
+        }}
       />
     </Block>
   );
