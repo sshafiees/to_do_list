c680c31c6769535c722f5caa64d4919700f02dc2
sina <alphaproject131313@gmail.com>
2025-09-19 15:14:38 +0330

    sort-ovwer

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import Block from '../layout/block';
 import NumberBlock from './numberBlock';
+import { TaskItemType } from '../../types/tasks';
+import { countTasksByStatus } from '../../utils/list';
 
-export default function Overview() {
+type OverviewProps = {
+  taskList: TaskItemType[];
+};
+
+export default function Overview({ taskList }: OverviewProps) {
   return (
     <Block>
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-bold">نمای کلی</h2>
         <div className="grid grid-cols-4 gap-4">
-          <NumberBlock number={5} title="مجموع" color="blue" />
-          <NumberBlock number={10} title="در انتظار" color="yellow" />
-          <NumberBlock number={15} title="در حال انجام" color="green" />
-          <NumberBlock number={15} title="تکمیل شده" color="red" />
+          <NumberBlock
+            number={taskList?.length || 0}
+            title="مجموع"
+            color="blue"
+          />
+          <NumberBlock
+            number={countTasksByStatus(taskList, 'notStarted')}
+            title="در انتظار"
+            color="yellow"
+          />
+          <NumberBlock
+            number={countTasksByStatus(taskList, 'inProgress')}
+            title="در حال انجام"
+            color="green"
+          />
+          <NumberBlock
+            number={countTasksByStatus(taskList, 'done')}
+            title="تکمیل شده"
+            color="red"
+          />
         </div>
       </div>
     </Block>
